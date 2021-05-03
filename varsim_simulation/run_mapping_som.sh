7f6c9aa20cb5b725c6579ac1ec4a502320a95851
Daniel Danciu <dd@daedalean.ai>
2021-05-03 09:13:45 +0200

    Replace Varsim with a simple script to generate reads using art_illumina

--- varsim_simulation/run_mapping_som.sh
+++
@@ -1,8 +0,0 @@
-for i in $(seq 0 699); do
-	file1="som_out/lane"$i".read1.fq.gz"
-	file2="som_out/lane"$i".read2.fq.gz"
-	samFile="cell_bams/som_cell"$i".sam"
-	echo bowtie2 -p 20 -x ref_index/GRCh38 -1 $file1 -2 $file2 -S $samFile >> log_mapping_som
-	bowtie2 -p 20 -x ref_index/GRCh38 -1 $file1 -2 $file2 -S $samFile
-	echo $i finished >> log_mapping_som
-done
