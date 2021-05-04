ddee7f04a8900b74772f4334f66c56baea9d4fe5
Daniel Danciu <dd@daedalean.ai>
2021-05-04 21:27:52 +0200

    Fix path handling in split.sh

--- varsim_simulation/split.sh
+++ varsim_simulation/split.sh
@@ -1,9 +1,10 @@
-name=$1
-pref="${name%.*}"
+source=$1 # contains full path to the source BAM file to be split
+filename=$(basename -- "${source}")  # extract file name from path
+pref="${filename%.*}"  # remove extension
 base_dir=$2
 
 for i in {1..22} X Y; do
   chunk_name="${base_dir}/aligned_cells_split/${pref}_chr${i}.bam"
-  samtools view -b "${base_dir}/aligned_cells/${name}" ${i} > "${chunk_name}"
+  samtools view -b "${source}" ${i} > "${chunk_name}"
   samtools index "${chunk_name}"
 done
