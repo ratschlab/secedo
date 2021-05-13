6a6759c6c1e5479ddb771bfc202b6b79820574e4
Daniel Danciu <dd@daedalean.ai>
2021-05-13 20:14:48 +0200

    Fix fasta dir

--- experiments/varsim/main.sh
+++ experiments/varsim/main.sh
@@ -126,7 +126,7 @@ function generate_reads() {
 
   for tumor_type in $(seq 1 "${n_tumor}"); do
     out_prefix=${out_dir}/tumor_${tumor_type}_
-    fasta="tumor-20K-${tumor_type}/tumor-20K-${tumor_type}.fa"
+    fasta="tumor-20K-${tumor_type}.fa"
 
     for batch in $(seq 0 ${step} $((n_cells-1))); do
       cmd="echo [$(date)] Copying data...; mkdir -p ${scratch_dir}; cp ${base_dir}/genomes/${fasta} ${scratch_dir}"
